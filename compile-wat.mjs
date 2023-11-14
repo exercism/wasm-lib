4e82ecf6a31a3fd909f32bec8616f7833c57bd11
Sean McBride <spmcbride1201@gmail.com>
2023-11-14 06:39:00 -0500

    feat: Improve error messages (#12)
    * feat: Improve error messages
    
    * fix: Stop dumping desugared form

--- compile-wat.mjs
+++ compile-wat.mjs
@@ -4,24 +4,29 @@ import initWabt from "wabt";
 let defaultWabtOptions = {
   exceptions: true,
   mutable_globals: true,
-  set_float_to_int: true,
+  sat_float_to_int: true,
   sign_extension: true,
   simd: true,
   threads: false,
+  function_references: true,
   multi_value: true,
   bulk_memory: true,
   reference_types: true,
   annotations: true,
+  code_metadata: true,
   gc: false,
+  memory64: false,
 };
 
 // Reads a *.wat file, parses to a *.wasm binary in-memory, and then compiles to a Wasm module we can instantiate
 export async function compileWat(watPath, optionsOverrides) {
   const wabt = await initWabt();
-  return wabt
-    .parseWat(watPath, readFileSync(watPath, "utf8"), {
-      ...defaultWabtOptions,
-      ...optionsOverrides,
-    })
-    .toBinary({ write_debug_names: true });
+  const module = wabt.parseWat(watPath, readFileSync(watPath, "utf8"), {
+    ...defaultWabtOptions,
+    ...optionsOverrides,
+  });
+  // Call validate on the text format so errors highlight source WAT
+  module.validate();
+
+  return module.toBinary({ write_debug_names: true });
 }
